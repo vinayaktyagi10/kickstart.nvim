4a9c8b171f481030e866615f3ec075668f38aa17
Vinayak Tyagi <vinayaktyagi.ed@gmail.com>
2025-05-28 07:26:50 +0530

    Updated plugin config and added lockfile

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  'neovim/nvim-lspconfig',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  'L3MON4D3/LuaSnip',
+
+  -- Treesitter for better Dart highlighting
+  { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+
+  -- Flutter tools for flutter commands + debugger integration
+  'akinsho/flutter-tools.nvim',
+}
